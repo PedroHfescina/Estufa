c7a21f7caa576b84affc18e72b31c8a64706a076
nome no git <pedrohenriquefescina@gmail.com>
2024-12-02 18:04:36 -0300

    Erro corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-import AddEstufa from "./pages/AddEstufa.jsx";
+
 import Login from "./pages/Login.jsx";
 import Config from "./pages/Config.jsx";
 
@@ -14,7 +14,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="AddEstufa" element={<AddEstufa />} />
           <Route path="Login" element={<Login />} />
           <Route path="Config" element={<Config />} />
         </Routes>
